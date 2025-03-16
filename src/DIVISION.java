c6490f95b59c8acbe0b877f72145410141d9d853
RAKSHAK-DESU <rakshak8788@gmail.com>
2025-03-16 16:03:39 +0530

    Updated code

--- src/DIVISION.java
+++ src/DIVISION.java
@@ -5,12 +5,12 @@ public class DIVISION {
         public static void main(String[] args){
             System.out.println("Enter 1st number");
             Scanner scanner= new Scanner(System.in);
-            float num2= scanner.nextFloat();
-
-            System.out.println("Enter 2nd Number");
             float num1= scanner.nextFloat();
 
-            float result= num2/num1;
+            System.out.println("Enter 2nd Number");
+            float num2= scanner.nextFloat();
+
+            float result= num1/num2;
             System.out.println(result);
 
         }
