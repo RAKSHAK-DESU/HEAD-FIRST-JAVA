8b555f10d894b7a2a138696b63ff4a814259c285
RAKSHAK-DESU <rakshakhingawe@gmail.com>
2025-03-16 15:51:16 +0530

    Added new class

---
+++ src/DIVISION.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DIVISION {
+
+        public static void main(String[] args){
+            System.out.println("Enter 1st number");
+            Scanner scanner= new Scanner(System.in);
+            float num1= scanner.nextFloat();
+
+            System.out.println("Enter 2nd Number");
+            float num2= scanner.nextFloat();
+
+            float result= num1/num2;
+            System.out.println(result);
+
+        }
+
+
+}
